ba5531a4a7720bd37fb5fbb1e769ea60d6bcb4e4
Clément Bruneteau <cbruneteau@epsi.fr>
2011-12-16 19:29:43 +0100

    it is good, sooo goood, so gooood, pampampampampam

--- Projet/myclipboard.c
+++ Projet/myclipboard.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
+#include "circbuf.h"
 
 volatile int idshm;
 
@@ -19,14 +20,15 @@ int main() {
 	key_t cle;
 	char * memoirePartagee;
 	
-	
 	cle = ftok("/etc/passwd", 1);
 	idshm = shmget(cle, 8192, IPC_CREAT | IPC_EXCL | 0760);
-	
 	memoirePartagee = shmat(idshm, 0, SHM_RND);
 	
+	circbuf_format(memoirePartagee, sizeof(memoirePartagee));
 	
-	strcpy(memoirePartagee, "");
+	
+	
+	//strcpy(memoirePartagee, "");
 	shmdt(memoirePartagee);
 	
 	
